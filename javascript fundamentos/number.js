9af6856188b2a68cead3c21aee836349ec2303a7
Sebastião Cosme <scosmeagostinho@hotmail.com>
2024-01-13 18:11:35 -0300

    novas aulas

---
+++ javascript fundamentos/number.js
@@ -0,0 +1,19 @@
+const peso1 = 1.0;
+const peso2 = Number('2.0');
+
+console.log(peso1 , peso2);
+console.log(Number.isInteger(peso1));
+console.log(Number.isInteger(peso2));
+
+const avaliacao1 = 9.871;
+const avaliacao2 = 6.871;
+
+const total = avaliacao1 * peso1 + avaliacao2 * peso2;
+const media = total / (peso1 + peso2);
+
+console.log(media.toFixed(2)); /*O método toFixed() formata um número utilizando notação de ponto fixo
+Ou seja, o metodo toFixed organiza a quantidade de  numeros que vai mostrar depois do ponto/virgula.
+*/
+console.log(media.toString(2));//O método toString() retorna uma string representando o objeto. Em binário
+console.log(typeof media);
+console.log(typeof Number);
---
+++ javascript fundamentos/tipagemFraca.js
@@ -0,0 +1,14 @@
+let exemplo = 'legal';
+console.log(exemplo);
+console.log(typeof exemplo);
+
+exemplo = 345678;
+
+console.log(exemplo);
+console.log(typeof exemplo);
+
+/*Em linguagens de programação existe as tipagens estática e dinamica, javascript é uma linguagem de
+tipagem dinamica, ou seja, diferente da tipagem estática que exige a declaração de quais dados
+poderão ser associados a cada variavel, a tipagem dinamica nao exige declaração de tipos de dados pois
+sao capazes de escolher que tipo utilizar dinamicamente para cada variavel.
+*/
