522ea497cf71fdb2118a9b79d16d1935eee631e8
scosme20 <scosmeagostinho@hotmail.com>
2024-01-14 12:34:11 -0300

    number

---
+++ javascript fundamentos/numberCuidados.js
@@ -0,0 +1,4 @@
+console.log(7/0);//Vai ser um resultado infinito
+console.log("10" / 2 ); // Mesmo como uma string, se o numero for inteiro ele será dividido
+console.log(0.1 + 0.7); //Não mostra um resultado exato
+console.log((10.345).toFixed(2));//Mostra duas casas depois do ponto
